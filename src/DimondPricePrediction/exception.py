c20dc674134b682536efe1623170e1583d97729a
Praveen <praveenku32k@gmail.com>
2024-01-28 18:55:52 +0530

    updating the Component file

--- src/DimondPricePrediction/exception.py
+++ src/DimondPricePrediction/exception.py
@@ -4,7 +4,7 @@ import sys
 class customexception(Exception):
     def __init__(self,error_message,error_details:sys):
         self.error_message = error_message
-        _,_,exc_tb = error_details.exc_info()
+        _,_,exc_tb = error_details.exc_info() # _._  formating of the code .
         
         self.lineno=exc_tb.tb_lineno
         self.file_name=exc_tb.tb_frame.f_code.co_filename
@@ -15,8 +15,7 @@ class customexception(Exception):
         
 
 
-    # string represention of the object.
-        
+    # string represention of the object. 
     def __str__(self):
         return "Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(
         self.file_name, self.lineno, str(self.error_message))
